79f3b829c1f7e1baf17e0f25b11f86a0d4cb16c7
max-translia <32696645+max-translia@users.noreply.github.com>
2019-08-21 11:09:36 +0800

    修改pom文件依赖指向的位置

--- liang-openfeign/src/main/java/com/liang/BootstrapApplication.java
+++ liang-openfeign/src/main/java/com/liang/BootstrapApplication.java
@@ -8,7 +8,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
-@EnableHystrix
+//@EnableHystrix
 public class BootstrapApplication {
 
     public static void main(String[] args) {
